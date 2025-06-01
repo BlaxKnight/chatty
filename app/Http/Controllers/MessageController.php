e70f698ad9834a45fe5f96a7ef9f146ca50fffb4
BlaxKnight <predator.blaxknight@gmail.com>
2025-06-01 08:15:06 +0330

    Removed Comments

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -11,8 +11,6 @@ use Illuminate\Support\Facades\Storage;
 use App\Models\Group;
 use App\Models\Message;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
